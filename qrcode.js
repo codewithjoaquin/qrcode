27cef9c81c9cee77098b74f0c85e474cdc360f03
codewithjoaquin <75036286+codewithjoaquin@users.noreply.github.com>
2023-10-04 02:05:48 -0400

    Update qrcode.js

--- qrcode.js
+++ qrcode.js
@@ -363,7 +363,7 @@ var QRCode;
 			this._bIsPainted = false;
 			this._elImage = document.createElement("img");
 			this._elImage.alt = "Scan me!";
-      this._elImage.class = "qrcode";
+                        this._elImage.setAttribute("class", "qrcode"); 
 			this._elImage.style.display = "none";
 			this._el.appendChild(this._elImage);
 			this._bSupportDataURI = null;
